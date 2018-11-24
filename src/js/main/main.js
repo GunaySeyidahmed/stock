b5e1f5835ae0656abebc4fb971d19bc6eb360f13
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2018-11-24 12:56:45 -0800

    one commit

---
+++ src/js/main/main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    function playVideo(el) {
+        var videoId = el.data('video');
+        var video = document.getElementById(videoId);
+
+        if (video.paused) {
+            // Play the video
+            video.play();
+            el.removeClass('paused').addClass('playing');
+        }
+        else {
+            // Pause the video
+            video.pause();
+            el.removeClass('playing').addClass('paused');
+        }
+    }
+    $(document).on('click', '.js-video-control', function(e) {
+        $(this).css('opacity','0.3');
+        playVideo($(this));
+        e.preventDefault();
+    });
+    $(".navToggle").click (function(){
+        $(this).parent().toggleClass("active");
+    });
+});
